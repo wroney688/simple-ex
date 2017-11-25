301dc3fdf9dc22f030a1b3d319662f559e1f255e
William N. Roney <wroney@roneymis.com>
2017-11-24 23:22:10 -0500

    scripting

--- runex.bat
+++ runex.bat
@@ -1,11 +1,27 @@
 
-kubectl run i1 --image=docker.io/wroney/ms-demo:v1.0.0 --labels="app=ms-demo,env=dev,build=v1.0.0" --expose=true --port=80
-kubectl run i2 --image=docker.io/wroney/ms-demo:v1.0.0 --labels="app=ms-demo,env=IST,build=v1.0.0" --expose=true --port=80
-kubectl run i3 --image=docker.io/wroney/ms-demo:v1.0.0 --labels="app=ms-demo,env=PT,build=v1.0.0" --expose=true --port=80
-kubectl run i4 --image=docker.io/wroney/ms-demo:v1.5.0 --labels="app=ms-demo,env=dev,build=v1.5.0" --expose=true --port=80
-kubectl run i5 --image=docker.io/wroney/ms-demo:v1.5.0 --labels="app=ms-demo,env=IST,build=v1.5.0" --expose=true --port=80
-kubectl run i6 --image=docker.io/wroney/ms-demo:v1.5.0 --labels="app=ms-demo,env=PT,build=v1.5.0" --expose=true --port=80
-kubectl run i7 --image=docker.io/wroney/ms-demo:v2.2.2 --labels="app=ms-demo,env=dev,build=v2.2.2" --expose=true --port=80
-kubectl run i8 --image=docker.io/wroney/ms-demo:v2.2.2 --labels="app=ms-demo,env=IST,build=v2.2.2" --expose=true --port=80
-kubectl run i9 --image=docker.io/wroney/ms-demo:v2.2.2 --labels="app=ms-demo,env=PT,build=v2.2.2" --expose=true --port=80
+kubectl run ms-demo-pt-rnd1 --replicas=5 --image=docker.io/wroney/ms-demo:v2.2.2 --labels="app=ms-demo,env=PT,build=v2.2.2" --port=80
+kubectl expose deployment ms-demo-pt-rnd1 --type=LoadBalancer --name=ms-demo-pt-rnd1
+kubectl run ms-demo-pt-rnd2 --replicas=5 --image=docker.io/wroney/ms-demo:v1.5.0 --labels="app=ms-demo,env=PT,build=v1.5.0" --port=80
+kubectl expose deployment ms-demo-pt-rnd2 --type=LoadBalancer --name=ms-demo-pt-rnd2
+kubectl run ms-demo-pt-rnd3 --replicas=5 --image=docker.io/wroney/ms-demo:v1.0.0 --labels="app=ms-demo,env=PT,build=v1.0.0" --port=80
+kubectl expose deployment ms-demo-pt-rnd3 --type=LoadBalancer --name=ms-demo-pt-rnd3
+
+kubectl run ms-demo-ist-rnd1 --replicas=3 --image=docker.io/wroney/ms-demo:v2.2.2 --labels="app=ms-demo,env=IST,build=v2.2.2" --port=80
+kubectl expose deployment ms-demo-ist-rnd1 --type=LoadBalancer --name=ms-demo-ist-rnd1
+kubectl run ms-demo-ist-rnd2 --replicas=3 --image=docker.io/wroney/ms-demo:v1.5.0 --labels="app=ms-demo,env=IST,build=v1.5.0" --port=80
+kubectl expose deployment ms-demo-ist-rnd2 --type=LoadBalancer --name=ms-demo-ist-rnd2
+kubectl run ms-demo-ist-rnd3 --replicas=3 --image=docker.io/wroney/ms-demo:v1.0.0 --labels="app=ms-demo,env=IST,build=v1.0.0" --port=80
+kubectl expose deployment ms-demo-ist-rnd3 --type=LoadBalancer --name=ms-demo-ist-rnd3
+
+
+kubectl run ms-demo-dev-rnd1 --replicas=1 --image=docker.io/wroney/ms-demo:v2.2.2 --labels="app=ms-demo,env=DEV,build=v2.2.2" --port=80
+kubectl expose deployment ms-demo-dev-rnd1 --type=LoadBalancer --name=ms-demo-dev-rnd1
+kubectl run ms-demo-dev-rnd2 --replicas=1 --image=docker.io/wroney/ms-demo:v1.5.0 --labels="app=ms-demo,env=DEV,build=v1.5.0" --port=80
+kubectl expose deployment ms-demo-dev-rnd2 --type=LoadBalancer --name=ms-demo-dev-rnd2
+kubectl run ms-demo-dev-rnd3 --replicas=1 --image=docker.io/wroney/ms-demo:v1.0.0 --labels="app=ms-demo,env=DEV,build=v1.0.0" --port=80
+kubectl expose deployment ms-demo-dev-rnd3 --type=LoadBalancer --name=ms-demo-dev-rnd3
+
+
+
+
 
---
+++ shutdownex.bat
@@ -0,0 +1,9 @@
+kubectl delete service ms-demo-pt-rnd1 ms-demo-pt-rnd2 ms-demo-pt-rnd3 
+kubectl delete service ms-demo-ist-rnd1 ms-demo-ist-rnd2 ms-demo-ist-rnd3 
+kubectl delete service ms-demo-dev-rnd1 ms-demo-dev-rnd2 ms-demo-dev-rnd3 
+kubectl delete deployment ms-demo-pt-rnd1 ms-demo-pt-rnd2 ms-demo-pt-rnd3 
+kubectl delete deployment ms-demo-ist-rnd1 ms-demo-ist-rnd2 ms-demo-ist-rnd3 
+kubectl delete deployment ms-demo-dev-rnd1 ms-demo-dev-rnd2 ms-demo-dev-rnd3 
+
+
+
